8064d48ee537e93c635b9f9367f2952b5e9ca271
ellerhew <lovedory43@naver.com>
2021-05-12 15:26:06 +0900

    project set up and add firebase

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,10 @@ import "./app.css";
 import Loginbox from "./components/loginbox";
 
 function App() {
+	const onClick = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+	};
+
 	return (
 		<>
 			<Loginbox />
